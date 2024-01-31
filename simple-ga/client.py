87969856bca2a1558580b836b524bfb896604249
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-30 22:01:44 -0500

    Add telemetry on genome and recvCounter

--- simple-ga/client.py
+++ simple-ga/client.py
@@ -20,6 +20,7 @@ runner.load()
 runner.run()
 runner.launch("dolaunch", nonblock=False)
 
+print("cycle counter =======================================================")
 memcpy_dtype = MemcpyDataType.MEMCPY_32BIT
 out_tensors_u32 = np.zeros((2, 2), np.uint32)
 
@@ -37,13 +38,51 @@ runner.memcpy_d2h(
     nonblock=False,
 )
 data = memcpy_view(out_tensors_u32, np.dtype(np.uint16))
+print(data)
+
+
+print("recv counter =========================================================")
+memcpy_dtype = MemcpyDataType.MEMCPY_32BIT
+out_tensors_u32 = np.zeros((2, 2), np.uint32)
+
+runner.memcpy_d2h(
+    out_tensors_u32,
+    runner.get_id("recvCounter"),
+    0,  # x0
+    0,  # y0
+    2,  # width
+    2,  # height
+    1,  # num wavelets
+    streaming=False,
+    data_type=memcpy_dtype,
+    order=MemcpyOrder.COL_MAJOR,
+    nonblock=False,
+)
+data = memcpy_view(out_tensors_u32, np.dtype(np.uint16))
+print(data)
+
+print("genome values ========================================================")
+memcpy_dtype = MemcpyDataType.MEMCPY_32BIT
+out_tensors_u32 = np.zeros((2, 2), np.float32)
+
+runner.memcpy_d2h(
+    out_tensors_u32,
+    runner.get_id("genome"),
+    0,  # x0
+    0,  # y0
+    2,  # width
+    2,  # height
+    1,  # num wavelets
+    streaming=False,
+    data_type=memcpy_dtype,
+    order=MemcpyOrder.COL_MAJOR,
+    nonblock=False,
+)
+data = memcpy_view(out_tensors_u32, np.dtype(np.float32))
+print(data)
 
 # runner.dump("corefile.cs1")
 runner.stop()
-
-# Ensure that the result matches our expectation
-print(data)
-print("SUCCESS!")
 
 # Ensure that the result matches our expectation
 print("SUCCESS!")
