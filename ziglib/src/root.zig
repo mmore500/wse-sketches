b5f517dea7abf68acdbe77472f589722c7308147
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-13 22:29:44 -0500

    Add modules to root

--- ziglib/src/root.zig
+++ ziglib/src/root.zig
@@ -1,2 +1,5 @@
 const hanoi = @import("hanoi.zig");
+const oeis = @import("oeis.zig");
 const pylib = @import("pylib.zig");
+const steady = @import("steady.zig");
+const tilted = @import("tilted.zig");
