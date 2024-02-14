c9dbb6ab51651d3171aedf36210ab0d2bd1421d8
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-14 01:44:56 -0500

    Impl, test tilted_sticky pick_deposition_site

--- ziglib/src/root.zig
+++ ziglib/src/root.zig
@@ -4,3 +4,4 @@ const oeis = @import("oeis.zig");
 const pylib = @import("pylib.zig");
 const steady = @import("steady.zig");
 const tilted = @import("tilted.zig");
+const tilted_sticky = @import("tilted_sticky.zig");
