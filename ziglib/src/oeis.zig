21bf3e1ff8382d1ef203895bb02dffd8d18dc12e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-13 22:53:43 -0500

    Refactor to use bit_length

--- ziglib/src/oeis.zig
+++ ziglib/src/oeis.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const pylib = @import("pylib.zig");
+
 pub fn get_a000295_value_at_index(n: u32) u32 {
     const adjustedN = n + 1;
     const shift: u5 = @intCast(adjustedN);
@@ -8,7 +10,7 @@ pub fn get_a000295_value_at_index(n: u32) u32 {
 
 pub fn get_a000295_index_of_value(v: u32) u32 {
     // Compute ansatz: the initial estimate based on the bit length of (v + 1)
-    const ansatz = 32 - @clz(v + 1) - 1;
+    const ansatz = pylib.bit_length(v + 1) - 1;
 
     // Calculate the value at the ansatz index and correct if necessary
     const correction = get_a000295_value_at_index(ansatz) > v;
