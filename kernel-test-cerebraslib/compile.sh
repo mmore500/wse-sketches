40eef797f66755111ff111cb57436ad34d78be08
mmore500 <mmore500.login@gmail.com>
2024-02-22 19:04:35 +0000

    Deploying to binder from @ mmore500/wse-sketches@2b9a2a62832770cc0e9707ebcd236a1ad1add2fa 🚀

--- kernel-test-cerebraslib/compile.sh
+++ kernel-test-cerebraslib/compile.sh
@@ -10,6 +10,9 @@ echo "CSLC ${CSLC}"
 trap "git checkout ./cerebraslib" EXIT
 rsync -rI "$(readlink -f cerebraslib)" .
 
+# remove any old output
+rm -rf out*
+
 # target a 1x1 region of interest
 # Every program using memcpy must use a fabric offset of 4,1, and if compiling
 # for a simulated fabric, must use a fabric dimension of at least
@@ -18,4 +21,24 @@ rsync -rI "$(readlink -f cerebraslib)" .
 # see https://sdk.cerebras.net/csl/tutorials/gemv-01-complete-program/
 # 9x4 because compiler says
 # RuntimeError: Fabric dimension must be at least 9-by-4
-"${CSLC}" layout.csl --fabric-dims=9,4 --fabric-offsets=4,1 --channels=1 --memcpy -o out --verbose
+test_module_paths="$(ls cerebraslib/test_*.csl)"
+num_tests="$(echo "${test_module_paths}" | wc -l)"
+echo "${num_tests} tests detected"
+
+MAX_PROCESSES=$([ -n "$CI" ] && echo 2 || echo 4)
+echo "Compiling ${num_tests} tests with up to ${MAX_PROCESSES} processes"
+
+for test_module_path in ${test_module_paths} END; do
+    if [ "${test_module_path}" = "END" ]; then
+        wait
+    else
+        cp "${test_module_path}" "cerebraslib/current_compilation_target.csl"
+        test_basename="$(basename -- "${test_module_path}")"
+        test_name="${test_basename%.csl}"
+          while [ "$(jobs -r | wc -l)" -ge "$MAX_PROCESSES" ]; do
+            wait -n
+        done
+        "${CSLC}" layout.csl --fabric-dims=9,4 --fabric-offsets=4,1 --channels=1 --memcpy -o "out_${test_name}" --verbose >/dev/null 2>&1 &
+        echo "${test_module_path}"
+    fi
+done | python3 -m tqdm --total "${num_tests}" --unit test --unit_scale --desc "Compiling"
