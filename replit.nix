3eef4c1f5fbc7b1758b6b95e6a43efd3a121f73b
Matthew Andres Moreno <mmore500.login@gmail.com>
2024-03-04 17:51:21 +0000

    Add replit files to version control

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+    deps = [
+      pkgs.pandoc
+      pkgs.texlive.combined.scheme-full
+      pkgs.python39Packages.pip
+      pkgs.nodejs
+      pkgs.editorconfig-checker
+      pkgs.python39Packages.editorconfig
+      pkgs.nodejs-16_x
+      pkgs.cowsay
+    ];
+}
