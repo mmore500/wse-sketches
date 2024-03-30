85ffe88ae9080100bcd3a24e248da9119ef41f44
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-03-30 18:59:11 -0400

    Import builtins first

--- kernel-async-ga/client.py
+++ kernel-async-ga/client.py
@@ -1,8 +1,9 @@
+import json
+import uuid
+
 import numpy as np
 import argparse
 import pandas as pd
-import json
-import uuid
 
 from cerebras.sdk.sdk_utils import memcpy_view
 from cerebras.sdk.runtime.sdkruntimepybind import (
