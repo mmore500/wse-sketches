6c6d04d440f86b042a583e7cde1260b2d856d11e
mmore500 <mmore500.login@gmail.com>
2024-04-10 20:21:25 +0000

    Deploying to binder from @ mmore500/wse-sketches@475f2ea74736cc0a37258fbb3de24fcf26be6acf 🚀

--- kernel-async-ga/execute.sh
+++ kernel-async-ga/execute.sh
@@ -5,5 +5,7 @@ set -e
 cd "$(dirname "$0")"
 
 echo "CS_PYTHON ${CS_PYTHON}"
+echo "ASYNC_GA_GENOME_FLAVOR ${ASYNC_GA_GENOME_FLAVOR}"
+echo "ASYNC_GA_EXECUTE_FLAGS ${ASYNC_GA_EXECUTE_FLAGS:-}"
 
-"${CS_PYTHON}" client.py --genomeFlavor "${ASYNC_GA_GENOME_FLAVOR}"
+"${CS_PYTHON}" client.py --genomeFlavor "${ASYNC_GA_GENOME_FLAVOR}" ${ASYNC_GA_EXECUTE_FLAGS:-}
