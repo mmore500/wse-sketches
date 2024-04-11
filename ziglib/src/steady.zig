77612818ce35e3d747034c23000fea1e4a427dac
Connor Yang <connoryang2005@gmail.com>
2024-04-10 20:17:29 -0400

    Convert get_num_segments to zig

--- ziglib/src/steady.zig
+++ ziglib/src/steady.zig
@@ -13,3 +13,11 @@ pub fn get_num_bins(surface_size: u32) u32 {
     // Right shift by 1 to divide by 2.
     return surface_size >> 1;
 }
+
+pub fn get_num_segments(surface_size: u32) u32 {
+    const num_bins = get_num_bins(surface_size);
+    // Calculate the bit length required to represent `num_bins`.
+    if (num_bins == 0) return 0; // Special case for 0, as its bit length is 0.
+    // Use the fact that bit length is the position of the highest set bit + 1.
+    return 32 - @clz(num_bins);
+}
--- ziglib/src/test_steady.zig
+++ ziglib/src/test_steady.zig
@@ -2,10 +2,25 @@ const std = @import("std");
 const expectEqual = std.testing.expectEqual;
 const steady = @import("steady.zig");
 
-test "get_num_bins" {
+test "test_get_num_bins" {
     try expectEqual(steady.get_num_bins(1), 0);
     try expectEqual(steady.get_num_bins(2), 1);
     try expectEqual(steady.get_num_bins(4), 2);
     try expectEqual(steady.get_num_bins(8), 4);
     try expectEqual(steady.get_num_bins(16), 8);
+}
+
+test "test_get_num_segments" {
+    try expectEqual(steady.get_num_segments(1), 0);
+    try expectEqual(steady.get_num_segments(2), 1);
+    try expectEqual(steady.get_num_segments(4), 2);
+    try expectEqual(steady.get_num_segments(8), 3);
+    try expectEqual(steady.get_num_segments(16), 4);
+    try expectEqual(steady.get_num_segments(32), 5);
+    try expectEqual(steady.get_num_segments(64), 6);
+    try expectEqual(steady.get_num_segments(128), 7);
+    try expectEqual(steady.get_num_segments(256), 8);
+    try expectEqual(steady.get_num_segments(512), 9);
+    try expectEqual(steady.get_num_segments(1024), 10);
+    try expectEqual(steady.get_num_segments(2048), 11);
 }
