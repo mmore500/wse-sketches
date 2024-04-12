57b1df1f4e743b5d803666709be9796d8e0eb299
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-04-12 16:58:47 -0400

    Switch to 4 hour run request

--- slurm/2024-04-12+lex214+doreconstruct.sh
+++ slurm/2024-04-12+lex214+doreconstruct.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --mem=128G
-#SBATCH --time=24:00:00
+#SBATCH --time=4:00:00
 #SBATCH --cpus-per-task=54
 #SBATCH --output="/jet/home/%u/joblog/id=%j+ext=.txt"
 
