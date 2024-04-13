f681a5d9bc79fadb01129b65bb5d847a12b192d7
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-04-13 18:38:45 -0400

    Set half hour timeout

--- binder/execute_notebooks.sh
+++ binder/execute_notebooks.sh
@@ -36,7 +36,7 @@ for notebook in "${script_dir}/"*.ipynb; do
   export NOTEBOOK_PATH="$(realpath "${notebook}")"
   jupyter nbconvert \
     --to notebook --execute --inplace \
-    --ExecutePreprocessor.timeout=600 \
+    --ExecutePreprocessor.timeout=1800 \
     "${notebook}"
 done
 
