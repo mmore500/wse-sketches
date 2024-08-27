c25f8ddd1cef8d85678fc4ad3a9c067e0b91b486
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-08-27 15:13:32 -0400

    Respect simulator min size

--- kernel-async-ga/generate_sample_tagged_populations.sh
+++ kernel-async-ga/generate_sample_tagged_populations.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 export ASYNC_GA_GENOME_FLAVOR="genome_hsurftiltedsticky_tagged"
 export ASYNC_GA_NCOL=4
 export ASYNC_GA_NCOL_SUBGRID=2
-export ASYNC_GA_NROW=2
+export ASYNC_GA_NROW=3
 export ASYNC_GA_NROW_SUBGRID=4  # should have no effect
 echo "ASYNC_GA_GENOME_FLAVOR ${ASYNC_GA_GENOME_FLAVOR}"
 for cmd in "export ASYNC_GA_GLOBAL_SEED=1 ASYNC_GA_NCYCLE_AT_LEAST=25" \
