3f978c5e554c227e813f5affc0f4c21305214d0d
mmore500 <mmore500.login@gmail.com>
2024-08-28 17:29:24 +0000

    Deploying to binder from @ mmore500/wse-sketches@e52b5dc8f4642f2dfeb4bfb6c4ae35cafc164896 🚀

--- kernel-async-ga/generate_sample_tagged_populations.sh
+++ kernel-async-ga/generate_sample_tagged_populations.sh
@@ -23,5 +23,6 @@ for cmd in "export ASYNC_GA_GLOBAL_SEED=1 ASYNC_GA_NCYCLE_AT_LEAST=25" \
 
 done
 
+gunzip *.gz
 find a=genomes+flavor=${ASYNC_GA_GENOME_FLAVOR}+*.csv \
   | python3 -m joinem "a=concatenated_genomes+flavor=${ASYNC_GA_GENOME_FLAVOR}+ext=.csv"
