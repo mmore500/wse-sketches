124a3fce60bf5b17736c83e77edd824a926a2f32
mmore500 <mmore500.login@gmail.com>
2024-08-28 06:16:26 +0000

    Deploying to binder from @ mmore500/wse-sketches@7090b88f4f93cc558b17d38f8d48dc7e06e4eb24 🚀

--- slurm/2024-08-17/2024-08-17+lex10+async-ga-hypermutator-512.sh
+++ slurm/2024-08-17/2024-08-17+lex10+async-ga-hypermutator-512.sh
@@ -116,6 +116,7 @@ cat > "${SBATCH_FILE}" << EOF
 #SBATCH --mem=32G
 #SBATCH --time=0:20:00
 #SBATCH --output="/jet/home/%u/joblog/id=%j+ext=.txt"
+#SBATCH --exclude=sdf-2
 
 set -e
 # newgrp GRANT_ID
